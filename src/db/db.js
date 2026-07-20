ad622f14c519e476de327b3397511b4f71235561
RodolfoVenegas <rodolfovenegas.es@gmail.com>
2026-07-20 10:07:13 -0400

    fix: update database connection configuration to include connectionString and ssl options

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,11 @@ import knex from 'knex';
 
 const db = knex({
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  },
+  pool: { min: 2, max: 10 },
 });
 
-export default db
+export default db;
